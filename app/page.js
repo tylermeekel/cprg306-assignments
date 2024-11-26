e53618d7ad8f03d3ad90a588c014132dc8d40254
Tyler Meekel <ty.meekel@gmail.com>
2024-11-25 18:31:35 -0700

    Add week 9

--- app/page.js
+++ app/page.js
@@ -11,6 +11,8 @@ export default function Home() {
         <li className="underline text-cyan-600 hover:text-cyan-300"><Link href="/week-5">Week 5</Link></li>
         <li className="underline text-cyan-600 hover:text-cyan-300"><Link href="/week-6">Week 6</Link></li>
         <li className="underline text-cyan-600 hover:text-cyan-300"><Link href="/week-7">Week 7</Link></li>
+        <li className="underline text-cyan-600 hover:text-cyan-300"><Link href="/week-8">Week 8</Link></li>
+        <li className="underline text-cyan-600 hover:text-cyan-300"><Link href="/week-9">Week 9</Link></li>
       </ul>
     </main>
   );
---
+++ app/week-9/meal-ideas.js
@@ -0,0 +1,45 @@
+"use client";
+
+const { useState, useEffect } = require("react");
+
+export default function MealIdeas({ingredient}) {
+    const [meals, setMeals] = useState([]);
+
+    async function fetchMealIdeas(ingredient) {
+        const url = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        if (data.meals === null) {
+            return [];
+        } else {
+            return data.meals;
+        }
+
+    }
+
+    async function loadMealIdeas() {
+        let meals = await fetchMealIdeas(ingredient);
+        setMeals(meals);
+        console.log(meals);
+    }
+
+    useEffect(() => {
+        loadMealIdeas();
+    }, [ingredient]);
+
+    return (
+        <div>
+            <h1 className="font-bold text-2xl">Meal Ideas</h1>
+            <ul className="flex gap-2 flex-col">
+                {
+                    meals.length > 0 ? 
+                    meals.map((meal) => 
+                        <li className="bg-slate-800 p-2 hover:bg-orange-800">{meal.strMeal}</li>
+                    ) : "There are no meals for the ingredient " + ingredient
+                }
+            </ul>
+        </div>
+    );
+}
